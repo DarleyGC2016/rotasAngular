d433453513aacbde8bad9b76e356d886936625eb
DarleyGC2016 <garciacardoso.darley@gmail.com>
2021-06-21 21:24:01 -0300

    aula#59

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos//curso-nao-encontrado/curso-nao-encontrado.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'cursos', component: CursosComponent},
-  {path:'curso/:id', component: CursoDetalheComponent},
-  {path:'naoEncontrado/:id', component: CursoNaoEncontradoComponent},
   {path:'login',component:LoginComponent},
   {path:'',component:HomeComponent}
 ];
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,18 +1,21 @@
+import { CursosRoutingModule } from './cursos.routing.module';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CursosComponent } from './cursos.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos.service';
 
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+
+import { MatTableModule} from '@angular/material/table';
+import { MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     imports:[
         CommonModule,
-        RouterModule,
-        MatTableModule
+        CursosRoutingModule,
+        MatTableModule,
+        MatButtonModule
     ],
     exports:[],
     declarations:[
