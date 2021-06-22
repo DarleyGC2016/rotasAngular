258b305cdd5e64abf4f9c3c253cb0a6d2d79953c
DarleyGC2016 <garciacardoso.darley@gmail.com>
2021-06-21 21:47:55 -0300

    Agora sim aula 59

--- src/app/cursos/cursos.routing.module.ts
+++ src/app/cursos/cursos.routing.module.ts
@@ -4,14 +4,14 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosComponent } from './cursos.component';
 
-const routes: Routes = [
+const cursosRotas: Routes = [
   {path:'cursos', component: CursosComponent},
   {path:'curso/:id', component: CursoDetalheComponent},
   {path:'naoEncontrado/:id', component: CursoNaoEncontradoComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
+    imports: [RouterModule.forChild(cursosRotas)],
     exports: [RouterModule]
   })
 
